ab48232965a3f2454f15a8c0273989d48fce091a
Mathias Lavaert <mlavaert@gmail.com>
2021-04-20 21:52:12 +0200

    Add dmenu

--- dwm/config.h
+++ dwm/config.h
@@ -63,7 +63,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run_recent", "-p", "Open: ", "-m", dmenumon, "-fn", dmenufont, "-nb", nord[0], "-nf", nord[4], "-sb", nord[9], "-sf", nord[0], NULL };
+static const char *dmenucmd[] = { "dmenu_run_recent", "-p", "Open: ", "-m", dmenumon, "-fn", NULL };
 
 #include "focusurgent.c"
 static Key keys[] = {
