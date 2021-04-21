01c735d2651188c1ae6d2305f30d1b0ab37d3e8e
Mathias Lavaert <mlavaert@gmail.com>
2021-04-21 20:43:27 +0200

    Fix little bug

--- dwm/config.h
+++ dwm/config.h
@@ -63,7 +63,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run_recent", "-p", "Open: ", "-m", dmenumon, "-fn", NULL };
+static const char *dmenucmd[] = { "dmenu_run_recent", "-p", "Open: ", "-m", dmenumon, NULL };
 
 #include "focusurgent.c"
 static Key keys[] = {
